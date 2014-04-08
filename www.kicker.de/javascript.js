144aa31d413c2d747b6153604678984e6a5b09f8
Stan Wiechers <whoisstan@gmail.com>
2014-04-07 21:04:01 -0400

    my message

--- www.kicker.de/javascript.js
+++ www.kicker.de/javascript.js
@@ -1,1 +1,18 @@
-true;
+
+//********************GLOBAL VARS******************************\\
+var images = [];
+var slideinterval;
+var numElements = 0;
+var scrollOffset;
+var adImage1 = 'https://fbcdn-profile-a.akamaihd.net/hprofile-ak-frc3/t1/p160x160/1395858_673932529303873_834510145_n.png';
+var coverimage = "https://scontent-b-iad.xx.fbcdn.net/hphotos-ash4/t1.0-9/1380812_673944225969370_531291205_n.png";
+var their_content;
+var year_left_off;
+var playing = false;
+var scrollOffset = -35;
+var tumblr_photo_source = "http://www.x-menmovies.com/images/home/featured/Xmen-Tout-Trailer.jpg";
+var tumblr_photo_caption = "Check out these 25 moments!";
+var tumblr_photo_click_thru = "facebook.com/xmenmovies";
+
+//********************UTIL +  HELPER FUNCS******************************\\
+
